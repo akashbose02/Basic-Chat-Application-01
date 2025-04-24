2b285b69786098602ad14a95b0e1b2c8024ba1e8
AKASH BOSE <akashbose2002@gmail.com>
2025-04-24 10:38:18 +0530

    added separate folders for frontend and backend , added expo for mobile app

---
+++ chat-backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins (insecure for prod)
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    console.log("Message received:", data);
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
